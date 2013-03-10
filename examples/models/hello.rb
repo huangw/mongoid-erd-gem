900fefebecb4585ec3c30e735b880b450d4358c3
Huang Wei <huangw@pe-po.com>
2013-03-10 15:24:22 +0100

    add enum example in hello.rb

--- examples/models/hello.rb
+++ examples/models/hello.rb
@@ -3,6 +3,7 @@
 class Hello # erd{color: red} Hello Class
   field :name, type:String # erd Name Field
   field :link, type:String # -> World
+  as_enum :gender, :male => 1, :female => 2  # erd: 性别
 
   def say # erd say hello world
 
